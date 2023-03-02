e5b7ae8df61e095a907d5640b365925f4c469b15
raul34 <raulsanfer8@gmail.com>
2023-03-02 10:03:02 +0000

    v1

---
+++ nombreJava.java
@@ -0,0 +1,6 @@
+public class nombreJava34 {
+	public static void main(String[] args) {
+		System.out.println("Me llamo Raul del Arco y he creado el servidor ubunserver34");
+		System.out.println("Vivo en San Fernando de Henares");
+}
+}
